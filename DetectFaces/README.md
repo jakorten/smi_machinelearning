a96738613f5f643eb9a8d56a98f4383c7579323d
J.A. Korten <johan.korten@han.nl>
2021-02-23 11:29:31 +0100

    Detect Faces using OpenCV

---
+++ DetectFaces/README.md
@@ -0,0 +1,14 @@
+Run the code like this:
+
+*python face_detect.py abba.png haarcascade_frontalface_default.xml*
+
+If you want to understand how the code works, the details are here:
+
+https://realpython.com/blog/python/face-recognition-with-python/
+
+EDIT: now works with OpenCV3.
+
+#Updates: 
+Update Feb 2021 for AD Smart Industry:
+I made some minor updates for Python3.
+J.A. Korten johan.korten@han.nl
---
+++ DetectFaces/face_detect.py
@@ -0,0 +1,35 @@
+import cv2
+import sys
+
+# Changes for Python3 J.A. Korten - 2021
+# Based on: https://static.realpython.com/guides/python-opencv-examples.pdf
+
+
+# Get user supplied values
+imagePath = sys.argv[1]
+cascPath = sys.argv[2]
+
+# Create the haar cascade
+faceCascade = cv2.CascadeClassifier(cascPath)
+
+# Read the image
+image = cv2.imread(imagePath)
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+# Detect faces in the image
+faces = faceCascade.detectMultiScale(
+    gray,
+    scaleFactor=1.2,
+    minNeighbors=5,
+    minSize=(20, 20)
+    #flags = cv2.CV_HAAR_SCALE_IMAGE
+)
+
+print ("Found "+str(len(faces))+" faces!")
+
+# Draw a rectangle around the faces
+for (x, y, w, h) in faces:
+    cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+cv2.imshow("Faces found", image)
+cv2.waitKey(0)
